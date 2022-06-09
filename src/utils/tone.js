037ac1630bbfb0186a569fa798f9301233a90047
mgrzhdnv <mgrazhdanov@gmail.com>
2022-06-09 19:48:24 +0300

    tone modified

--- src/utils/tone.js
+++ src/utils/tone.js
@@ -2,17 +2,17 @@ import * as Tone from 'tone'
 
 const synth = new Tone.PolySynth(Tone.AMSynth).toDestination()
 synth.set({
-  harmonicity: 2,
+  harmonicity: 1, // 2 (2 - second oscillator octave higher)
   oscillator: {
     type: 'amsine2',
     modulationType: 'sine',
-    harmonicity: 1.01
+    harmonicity: 1 //1.01
   },
   envelope: {
     attack: 0.006,
     decay: 4,
     sustain: 0.04,
-    release: 1.2
+    release: 2 // 1.2
   },
   modulation: {
     volume: 13,
@@ -24,7 +24,7 @@ synth.set({
     attack: 0.006,
     decay: 0.2,
     sustain: 0.2,
-    release: 0.4
+    release: 1 //0.4
   }
 })
 
