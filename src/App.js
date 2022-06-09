e100010fdd2b9577de9e79ea1366c2d3e572b082
mgrzhdnv <mgrazhdanov@gmail.com>
2022-06-09 19:26:53 +0300

    connected select to range and layout, styled select, comp refactor

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import { Select } from '@mantine/core'
-import Piano from './components/Piano'
+import { Box, RangeSlider, Select } from '@mantine/core'
 import playSynth from './utils/tone'
 import { bayanLayout, pianoLayout, pianoSimple } from './utils/keyboardLayouts'
+import { whiteKeysFull } from './utils/pianoArray'
+import KeyboardSVG from './components/KeyboardSVG'
 // import { fullKeyboard } from './utils/pianoArray'
 
 function App() {
   // const [pressedKeys, setPressedKeys] = useState([])
-  const [value, setValue] = useState('Piano')
-  const [layout, setLayout] = useState(pianoLayout)
-  const [range, setRange] = useState([16, 34])
+  const [value, setValue] = useState('Piano (simple)')
+  const [layout, setLayout] = useState(pianoSimple)
+  const [range, setRange] = useState([13, 28])
 
+  // For range
+  const indexWhiteKeyMap = { ...whiteKeysFull }
+  const marks = []
+  whiteKeysFull.forEach((key, index) => {
+    if (key[0] === 'C' || key === 'A0') {
+      marks.push({ value: index, label: key })
+    }
+  })
+  // props from range:
+  const startKey = indexWhiteKeyMap[range[0]]
+  const endKey = indexWhiteKeyMap[range[1]]
+
+  // Select layout and range maps
   const valueToLayout = { Piano: pianoLayout, Bayan: bayanLayout, 'Piano (simple)': pianoSimple }
+  const valueToRange = { Piano: [12, 35], Bayan: [16, 37], 'Piano (simple)': [13, 28] }
 
-  const setSelect = e => {
+  const handleSelect = e => {
     setValue(e)
     setLayout(valueToLayout[e])
-    /*     if (e === 'bayanLayout') {
-      setLayout(() => bayanLayout)
-    } else if (e === 'pianoLayout') {
-      setLayout(pianoLayout)
-    } else if (e === 'pianoSimple') {
-      setLayout(pianoSimple)
-    } */
+    setRange(valueToRange[e])
   }
 
   const handleKeyDown = e => {
@@ -52,7 +61,6 @@ function App() {
     }
   }
 
-  // WHY IT TRIGGERS FOR EVERY KEYBOARD EVENT?! (when without [])
   useEffect(() => {
     window.addEventListener('keydown', handleKeyDown, false)
     window.addEventListener('keyup', handleKeyUp, false)
@@ -69,13 +77,35 @@ function App() {
   return (
     <>
       <div className="App">
-        <Piano range={range} />
-        <Select
-          value={value}
-          onChange={e => setSelect(e)}
-          data={['Piano', 'Piano (simple)', 'Bayan']}
-          label="Keyboard layout"
-        />
+        <Box id="sliderContainer">
+          <RangeSlider
+            color="dark"
+            size="xl"
+            min={0}
+            max={51}
+            minRange={7}
+            precision={0}
+            value={range}
+            label={value => `${indexWhiteKeyMap[value]}`}
+            onChange={setRange}
+            marks={marks}
+          />
+        </Box>
+        <KeyboardSVG startKey={startKey} endKey={endKey} />
+        <Box id="selectContainer">
+          <Select
+            value={value}
+            onChange={e => handleSelect(e)}
+            data={['Piano', 'Piano (simple)', 'Bayan']}
+            label="Keyboard layout"
+          />
+        </Box>
+        {/*         <h4>Piano range:&nbsp;
+      <em>
+        {fullKeyboard.indexOf(indexWhiteKeyMap[range[1]]) - fullKeyboard.indexOf(indexWhiteKeyMap[range[0]]) + 1}
+        &nbsp;keys
+      </em></h4> */}
+        {/* <h2 id="chords">Chord: {chord}</h2> */}
       </div>
     </>
   )
--- src/components/Piano.js
+++ src/components/Piano.js
@@ -1,56 +1,9 @@
-import React, { useState } from 'react'
 // import { detect } from '@tonaljs/chord-detect'
-import { Box } from '@mantine/core'
-import { RangeSlider } from '@mantine/core'
-import KeyboardSVG from './KeyboardSVG'
-import { whiteKeysFull } from '../utils/pianoArray'
 
-const Piano = () => {
-  const [range, setRange] = useState([16, 34])
-
-  const indexWhiteKeyMap = { ...whiteKeysFull }
-
-  const marks = []
-  whiteKeysFull.forEach((key, index) => {
-    if (key[0] === 'C' || key === 'A0') {
-      marks.push({ value: index, label: key })
-    }
-  })
-
-  // props:
-  const startKey = indexWhiteKeyMap[range[0]]
-  const endKey = indexWhiteKeyMap[range[1]]
+const Piano = ({ startKey, endKey }) => {
   // const detectedChord = detect(chord).join(' - ')
 
-  return (
-    <>
-      <Box id="sliderContainer">
-        <RangeSlider
-          color="dark"
-          size="xl"
-          min={0}
-          max={51}
-          minRange={7}
-          precision={0}
-          value={range}
-          label={value => `${indexWhiteKeyMap[value]}`}
-          onChange={setRange}
-          marks={marks}
-        />
-        {/*         <h4>
-          Piano range:&nbsp;
-          <em>
-            {fullKeyboard.indexOf(indexWhiteKeyMap[range[1]]) -
-              fullKeyboard.indexOf(indexWhiteKeyMap[range[0]]) +
-              1}
-            &nbsp;keys
-          </em>
-        </h4> */}
-        {/* <h2 id="chords">Chord: {chord}</h2> */}
-      </Box>
-      <KeyboardSVG startKey={startKey} endKey={endKey} />
-    </>
-  )
+  return null
 }
 
 export default Piano
